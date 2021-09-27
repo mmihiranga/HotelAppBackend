5e8af070ed71a92c9139e4115c1e8c5206286477
nnj16 <nethsera@gmail.com>
2021-09-27 21:46:28 +0530

    Add search function for food api

--- src/api/food.api.js
+++ src/api/food.api.js
@@ -3,10 +3,11 @@ const router = express.Router();
 const FoodController = require('../controller/food.controller');
 
 module.exports = function (){
-    router.get('/:name', FoodController.getFoodFromName);
+    router.get('/:id', FoodController.getFoodFromCode);
     router.post('/create', FoodController.createFood);
     router.put('/update', FoodController.updateFood);
     router.delete('/delete/:id', FoodController.deleteFood);
     router.get('/', FoodController.getAllFoods);
+    router.get('/category/:id', FoodController.getFoodsByCategory);
     return router;
 }
